c8f428c36425d024339da98f0a9ae6498b09dd95
Shane Tanner <shanetanner03@gmail.com>
2022-11-02 16:06:07 -0500

    Moved the logic for the like and dislike buttons over to the Post.jsx file + Made the like and dislike buttons work as instructed + Mad a few changes to the css of Post.css

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,18 +1,40 @@
-import LikeDislikeButton from "../LikeDislikeButton/LikeDislikeButton";
+import React, { useState } from 'react';
+import './Post.css'
 
 const Post = (props) => {
+
+  const [likeButton, setLikeButton] = useState("inactive");
+
+  const [dislikeButton, setDislikeButton] = useState("inactive");
+
+  function handleClick1() {
+      if(likeButton === "inactive") {
+          setLikeButton("active");
+          setDislikeButton("inactive");
+      }
+      else {
+          setLikeButton("inactive");
+      }
+  }
+
+  function handleClick2() {
+      if(dislikeButton === "inactive") {
+          setDislikeButton("active");
+          setLikeButton("inactive");
+      }
+      else {
+          setDislikeButton("inactive");
+      }
+  }
+
   return (
     <div className="post-divide">
-      <div>
-        <h5>{props.postData.name}</h5>
-      </div>
-      <div>
+        <h5 className='partition'>{props.postData.name}</h5>
         <p>{props.postData.post}</p>
-      </div>
       <div className="button-placement">
-        <LikeDislikeButton />
+        <button className={likeButton} onClick={handleClick1}>Like</button>
+        <button className={dislikeButton} onClick={handleClick2}>Dislike</button>
       </div>
-      <hr></hr>
     </div>
   );
 };
